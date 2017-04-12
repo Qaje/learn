9f5cea1c3d9c3b09cf2fb36f2840d0f79b4f2dd8
qaje <javierpal3232@gmail.com>
2017-04-11 23:28:51 -0700

    with Users

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::table('users')->truncate();
+
+    	factory(App\User::class)->create([
+        	'name' => 'Javier',
+        	'email' => 'admin@styde.net',
+         	'role' => 'admin',
+         	'password'=> ''
+        	]);
+        factory(App\User::class, 49)->create();
+    }
+}
